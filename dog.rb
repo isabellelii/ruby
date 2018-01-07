3c5d15a542ea7e571c07f870af8050962f09d45f
isabellelii <34246127+isabellelii@users.noreply.github.com>
2018-01-07 20:48:16 +0100

    Ruby exercise
    Dog.rb

---
+++ dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  attr_accessor :breed, :name
+
+  def initialize(breed, name)
+    @breed = breed
+    @name = name
+  End
+
+  def wag_tail
+    puts "Tail wagging"
+  End
+End
+
+class Dog
+
+  [...]
+
+  def wag_tail
+    puts "Tail wagging"
+  End
+End
